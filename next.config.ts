aaca9267a0d76c1f6ba4c32e94c0f3397b8e36ba
nataliaYarr <n.yarysheva@gmail.com>
2025-08-30 20:28:59 -0700

    .

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // ✅ This allows production builds to complete even with TS errors
+   
     ignoreBuildErrors: true,
   },
   eslint: {
-    // (Optional) This allows production builds even if you have ESLint errors
+   
     ignoreDuringBuilds: true,
   },
 };
